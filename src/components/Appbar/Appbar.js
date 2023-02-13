4f18c0c4608b8017ebaa06a0f707b4b7d4985b28
IO <ilaria.posca@gmail.com>
2023-02-13 11:15:30 +0100

    titles refactoring

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid} from '@material-ui/core';
 import mainLogo from "../../assets/logo-utero.svg";
 
 
