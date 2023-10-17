59da06aa84b0b626d3962caa688379540ba85193
IO <ilaria.posca@gmail.com>
2023-10-17 15:57:20 +0200

    feat/style fix header

--- src/components/Appbar/Appbar.js
+++ src/components/Appbar/Appbar.js
@@ -1,23 +1,30 @@
 import * as React from 'react';
-import { Grid} from '@material-ui/core';
+import { Grid, useMediaQuery, useTheme } from '@material-ui/core';
 import mainLogo from "../../assets/logo-utero.svg";
 
-
 export default function BasicMenu() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+  const logoStyle = {
+    height: isMobile ? '80px' : '100%', 
+  };
+  const subTitleStyle = {
+    width: isMobile ? '50%' : '100%', 
+    margin:'0 auto',
+  };
 
   return (
     <div>
-      <Grid container>
-            <Grid item xs={3} lg={1}>
-            <img src={mainLogo} alt="Claudia Gizzi:osteopata ed esperta PMA" height={80} />
-            </Grid>
-            <Grid item xs={6} lg={10}>
-              <h1> Claudia Gizzi</h1>
-              <h2>Osteopata ed Esperta in fertilit&agrave;</h2>
-            </Grid>
+      <Grid container alignItems='center'>
+        <Grid item xs={12} lg={1}>
+          <img src={mainLogo} alt="Claudia Gizzi: osteopata ed esperta PMA" style={logoStyle} />
+        </Grid>
+        <Grid item xs={12} lg={10}>
+          <h1> Claudia Gizzi</h1>
+          <h2 style={subTitleStyle} >Osteopata ed Esperta  in fertilit&agrave;</h2>
+        </Grid>
       </Grid>
-      
     </div>
   );
 }
